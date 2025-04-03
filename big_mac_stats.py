86d30f3c196dc751d85af5a1049f7906114aaaa9
FilinMen <fiilinkadabra@tutamail.com>
2025-04-03 17:31:17 +0300

    kaif

--- big_mac_stats.py
+++ big_mac_stats.py
@@ -1,6 +1,6 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 from log import log
 
 class BigMacStatistic:
@@ -10,12 +10,24 @@ class BigMacStatistic:
         df = pd.read_csv('BigmacPrice.csv')
         country_col = 'name'
         price_col = 'dollar_price'
-        selected_countries = ['China', 'Russia', 'Euro area', 'United States','Britain', 'Canada', 'Australia', 'Japan', 'Switzerland', 'Sweden','Norway', 'Brazil', 'Mexico','South Africa', 'Turkey', 'India', 'Thailand','Saudi Arabia', 'United Arab Emirates']
+        selected_countries = ['China', 'Russia', 'United States','Canada', 'Australia', 'Japan', 'Switzerland', 'Sweden','Norway', 'Brazil', 'Mexico', 'South Africa', 'Turkey', 'India', 'Thailand', 'Saudi Arabia', 'United Arab Emirates', 'Denmark', 'Colombia','Singapore','Sri Lanka','Peru','Indonesia','Chile','Costa Rica','Uruguay']
         df_filtered = df[df[country_col].isin(selected_countries)] \
-                      .drop_duplicates(subset=[country_col], keep='last')
-        df_filtered = df_filtered.sort_values(price_col, ascending=False)
+                      .drop_duplicates(subset=[country_col], keep='last') \
+                      .sort_values(price_col, ascending=False)
         
-    
+        
+        fig_map = px.choropleth(
+            df_filtered,
+            locations=country_col,  
+            locationmode='country names',
+            color=price_col,
+            hover_name=country_col,
+            color_continuous_scale='Inferno',
+            title='РЯД 1',
+            labels={'dollar_price': 'Price'})
+        fig_map.show()
+        
+       
         fig, ax = plt.subplots(figsize=(10, 6))
         df_filtered.plot(kind='barh', x=country_col, y=price_col, ax=ax,
                         title='РЯД 1', 
@@ -23,10 +35,8 @@ class BigMacStatistic:
         fig.patch.set_facecolor('#0000B2')
         ax.set_facecolor('#0000B2')
         
-
         for i, v in enumerate(df_filtered[price_col]):
             ax.text(v + 0.1, i, f"${v:.2f}", color='white', va='center', fontsize=10)
-        
         
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
@@ -37,11 +47,3 @@ class BigMacStatistic:
         ax.title.set_color('white')
         plt.tight_layout()
         plt.show()
-
-
-
-
-
-
-       
-         
